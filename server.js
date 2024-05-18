7942fe69662fb72c9bcd13227e09f8f7002a2823
MinhYii <minhyii.dev@gmail.com>
2024-05-18 11:09:38 +0700

    FIX

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        name: "Ok",
+    });
+});
+app.listen(3000, () => {
+    console.log(`Listen at PORT:3000`);
+});
